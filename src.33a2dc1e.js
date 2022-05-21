682bf85d8ce8caabe260c6a18d844fa232835dc3
GodzilaSanya <godzilasanya@gmail.com>
2022-05-21 16:18:47 +0000

    Deploying to gh-pages from @ GodzilaSanya/test_parcel@2429e6fac891fcaa91863b3afd021fb386cc0529 🚀

--- src.33a2dc1e.js
+++ src.33a2dc1e.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test_parcel/src.53e67f59.js.map
+//# sourceMappingURL=/test_parcel/src.33a2dc1e.js.map
